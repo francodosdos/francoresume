52f40d1a9c318509fe54a0a5cb00b7772e96c3e3
francodosdos <112530176+francodosdos@users.noreply.github.com>
2023-03-23 15:06:01 +0800

    Version Beta 1.4.5 - More Info

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: 11.6978352, lng: 122.6217542};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,6 +10,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Cebu, PH' // Title Location
     });
 }
